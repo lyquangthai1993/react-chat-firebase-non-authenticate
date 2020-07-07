6a586906c3f39a93e8a17f847b1f6f9178c15629
Lý Quang Thái <lyquangthai1993@gmail.com>
2020-07-07 11:56:56 +0700

    update

--- src/Config/MyFirebase.js
+++ src/Config/MyFirebase.js
@@ -1,18 +1,20 @@
-import firebase from 'firebase'
+import firebase from "firebase";
 
 const config = {
-    apiKey: 'AIzaSyAC9ZSyvTxU2o5W0jcL0hDvzS4utLuicE8',
-    authDomain: 'flutterchatdemo.firebaseapp.com',
-    databaseURL: 'https://flutterchatdemo.firebaseio.com',
-    projectId: 'flutterchatdemo',
-    storageBucket: 'flutterchatdemo.appspot.com',
-    messagingSenderId: '347976604232'
-}
-firebase.initializeApp(config)
+  apiKey: "AIzaSyAMqOCTERYQf7LrvhauwxDtMg4Jkpz0zNE",
+  authDomain: "fatih-chat-dev-web.firebaseapp.com",
+  databaseURL: "https://fatih-chat-dev-web.firebaseio.com",
+  projectId: "fatih-chat-dev-web",
+  storageBucket: "fatih-chat-dev-web.appspot.com",
+  messagingSenderId: "61852437446",
+  appId: "1:61852437446:web:5bd25526a0a26c336c80c7",
+  measurementId: "G-SS8MR325NJ",
+};
+firebase.initializeApp(config);
 firebase.firestore().settings({
-    timestampsInSnapshots: true
-})
+  timestampsInSnapshots: true,
+});
 
-export const myFirebase = firebase
-export const myFirestore = firebase.firestore()
-export const myStorage = firebase.storage()
+export const myFirebase = firebase;
+export const myFirestore = firebase.firestore();
+export const myStorage = firebase.storage();
